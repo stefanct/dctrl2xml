72f75a1414846c6e39187007ece28e43caf9e303
mrfrost <mrfrost@2db7d633-71b8-0310-b1c1-c0b6732fd93e>
2005-01-10 04:39:12 +0000

    git-svn-id: http://localhost/svn/software/dctrl2xml/trunk@329 2db7d633-71b8-0310-b1c1-c0b6732fd93e

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ from distutils.core import setup
 
 args = {
     'name': 'dctrl2xml',
+    'version': '0.1',
+    'description': '',
+    'author': 'Frank S. Thomas',
+    'author_email': 'frank@thomas-alfeld.de',
+    'license': 'GNU GPL'
+    'scripts': (
+	'dctrl2xml'
+    )
 }
 
 if __name__ == '__main__':
