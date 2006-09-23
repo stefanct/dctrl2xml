e515113d38749e6a34f898bbaf96a3345a7109a1
mrfrost <mrfrost@2db7d633-71b8-0310-b1c1-c0b6732fd93e>
2006-09-23 16:24:16 +0000

    git-svn-id: http://localhost/svn/mrfrost/radix/forum/code/dctrl2xml/trunk@1621 2db7d633-71b8-0310-b1c1-c0b6732fd93e

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from distutils.core import setup
 
 args = {
     'name': 'dctrl2xml',
-    'version': '0.4',
+    'version': '0.5',
     'description': 'Debian package control file to XML converter',
     'author': 'Frank S. Thomas',
     'author_email': 'frank@thomas-alfeld.de',
-    'url': 'http://www.thomas-alfeld.de/frank/download/debian/dctrl2xml/',
+    'url': 'http://frank.thomas-alfeld.de/download/debian/dctrl2xml/',
     'license': 'GNU GPL',
     'scripts': (
         'scripts/dctrl2xml',
