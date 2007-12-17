5fe00d0cbd84b36ac7fcc05f99f67fc1d149a5bc
Frank S. Thomas <frank@thomas-alfeld.de>
2007-12-17 16:18:27 +0100

    Incorporated all suggestions from Justin B Rye's <jbr@edlug.org.uk> description review.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 args = {
     'name': 'dctrl2xml',
     'version': '0.7',
-    'description': 'convert Debian control data to XML',
+    'description': 'Debian control data to XML converter',
     'author': 'Frank S. Thomas',
     'author_email': 'fst@debian.org',
     'scripts': ['dctrl2xml'],
