6fe786c25e59f0ac5c020df362e0e1df001aea5d
Frank S. Thomas <frank@thomas-alfeld.de>
2007-12-22 18:35:06 +0100

    Bumped version to 0.9.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 args = {
     'name': 'dctrl2xml',
-    'version': '0.8',
+    'version': '0.9',
     'description': 'Debian control data to XML converter',
     'author': 'Frank S. Thomas',
     'author_email': 'fst@debian.org',
